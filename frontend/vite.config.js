3bf46940adf8f46c834f4cf85e0c6357fb623925
ishans2404 <ishans2404@gmail.com>
2026-04-21 23:06:26 +0530

    init

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pmloading/',
+  server: {
+    proxy: {
+      '/api-proxy': {
+        target: 'https://bspapp.sail-bhilaisteel.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api-proxy/, ''),
+        secure: true,
+      }
+    }
+  },
+  base: '/pmloading/'
 })
